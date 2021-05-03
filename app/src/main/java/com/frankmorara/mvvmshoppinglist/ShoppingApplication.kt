d5f4c2bd8a699349c1d491f253c7e405be3f2c96
frankmorara <franklinenyabuto11@gmail.com>
2021-05-03 16:52:47 +0300

    Dependency injenction using Kodein

---
+++ app/src/main/java/com/frankmorara/mvvmshoppinglist/ShoppingApplication.kt
@@ -0,0 +1,33 @@
+package com.frankmorara.mvvmshoppinglist
+
+import android.app.Application
+import com.frankmorara.mvvmshoppinglist.data.db.ShoppingDatabase
+import com.frankmorara.mvvmshoppinglist.data.repos.ShoppingRepository
+import com.frankmorara.mvvmshoppinglist.ui.shoppinglist.ShoppingViewModelFactory
+import kotlinx.coroutines.InternalCoroutinesApi
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.androidXModule
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
+import org.kodein.di.generic.provider
+import org.kodein.di.generic.singleton
+
+class ShoppingApplication: Application(), KodeinAware {
+
+    @InternalCoroutinesApi
+    override val kodein: Kodein = Kodein.lazy {
+        import(androidXModule(this@ShoppingApplication))
+        bind() from singleton { ShoppingDatabase(instance()) }
+        bind() from singleton {
+            ShoppingRepository(
+                instance()
+            )
+        }
+        bind() from provider {
+            ShoppingViewModelFactory(
+                instance()
+            )
+        }
+    }
+}
